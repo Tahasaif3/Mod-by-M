3464461582e342049f534185ffa6edb700c18d3b
Taha Saif <tahasaifsurhio454@gmail.com>
2025-02-07 17:42:13 +0500

    Update FooterBottom.tsx

--- src/components/FooterBottom.tsx
+++ src/components/FooterBottom.tsx
@@ -8,7 +8,7 @@ const FooterBottom = () => {
   return (
     <div className="overflow-hidden  w-full h-[53px] bg-[#E7E4F8] flex items-center justify-between px-6 py-4 sm:px-12 md:px-24 lg:px-52">
       <div className="text-sm text-[#9DA0AE] text-[14px] sm:text-[16px]">
-        <p>©Jawaid Ali - All Rights Reserved</p>
+        <p>©Hekto- All Rights Reserved</p>
       </div>
 
       <div className="flex space-x-4">
